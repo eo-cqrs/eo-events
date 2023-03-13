0df7fa0b9bc4956f4d7afdce3b1bda8935c3c34f
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-03-13 14:22:00 +0300

    #6

---
+++ src/main/java/io/github/eocqrs/qce/Event.java
@@ -0,0 +1,15 @@
+package io.github.eocqrs.qce;
+
+/**
+ * @author Aliaksei Bialiauski (abialiauski.dev@gmail.com)
+ * @since 0.0.0
+ */
+public interface Event {
+
+  /**
+   * @todo #6:5m/DEV remove dummy implementation
+   */
+  final class S implements Event {
+
+  }
+}
---
+++ src/test/java/io/github/eocqrs/qce/EventTest.java
@@ -0,0 +1,19 @@
+package io.github.eocqrs.qce;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case for {@link Event}
+ *
+ * @since 0.0.0
+ */
+class EventTest {
+
+  /**
+   * @todo #6:5m/DEV remove dummy test for cov checks
+   */
+  @Test
+  void simple() {
+    final Event.S simple = new Event.S();
+  }
+}
